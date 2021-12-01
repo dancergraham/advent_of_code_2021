3f39502d703ffcf281815a2730ba99ae2f39d6ad
Graham Knapp <graham.knapp@acernis.fr>
2021-12-01 21:29:59 +0100

    Add solution day 00 python template

---
+++ day00/day00.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+test_input = """"""
+
+
+def part_1(puzzle_input: str) -> float:
+    parsed = (int(x) for x in puzzle_input.splitlines())
+    answer = 0
+    for current in parsed:
+        ...
+    return answer
+
+
+def part_2(puzzle_input: str) -> float:
+    parsed = (int(x) for x in puzzle_input.splitlines())
+    answer = 0
+    for current in parsed:
+        ...
+    return answer
+
+
+def test_part_1():
+    assert part_1(test_input) == 0
+
+
+def test_part_2():
+    assert part_2(test_input) == 0
+
+
+def main():
+    puzzle_input = Path("input.txt").read_text()
+    print(part_1(puzzle_input))
+    print(part_2(puzzle_input))
+
+
+if __name__ == "__main__":
+    main()
