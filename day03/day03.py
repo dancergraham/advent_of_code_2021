3907da547b2d6f754febd13f2e6d8940a5569cc1
Graham Knapp <graham.knapp@acernis.fr>
2021-12-03 08:43:42 +0100

    Day 03 part 1 in Python

--- day03/day03.py
+++ day03/day03.py
@@ -1,14 +1,30 @@
 from pathlib import Path
 
-test_input = """"""
+test_input = """00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010
+"""
 
 
 def part_1(puzzle_input: str) -> float:
-    parsed = (int(x) for x in puzzle_input.splitlines())
-    answer = 0
-    for current in parsed:
-        ...
-    return answer
+    numbers = [x for x in puzzle_input.splitlines()]
+    counter = [0 for bit in numbers[0]]
+    for number in numbers:
+        for i, digit in enumerate(number):
+            counter[i] += int(digit)
+    values = len(numbers)
+    gamma = int("".join(str(int((digit / values) > 0.5)) for digit in counter), 2)
+    epsilon = int("".join(str(int((digit / values) < 0.5)) for digit in counter), 2)
+    return gamma * epsilon
 
 
 def part_2(puzzle_input: str) -> float:
@@ -20,7 +36,7 @@ def part_2(puzzle_input: str) -> float:
 
 
 def test_part_1():
-    assert part_1(test_input) == 0
+    assert part_1(test_input) == 198
 
 
 def test_part_2():
