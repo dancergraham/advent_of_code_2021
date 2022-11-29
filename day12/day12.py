e0560baa0f216ac25a85c45b947ffd0145e396a6
Graham Knapp <graham.knapp@gmail.com>
2022-11-29 20:29:31 +0100

    Add day12 part 2 in Python
    Signed-off-by: Graham Knapp <graham.knapp@gmail.com>

--- day12/day12.py
+++ day12/day12.py
@@ -8,6 +8,36 @@ b-d
 A-end
 b-end"""
 
+test_input_2 = """dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc"""
+
+test_input_3 = """fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW"""
+
 
 def part_1(puzzle_input: str) -> float:
     parsed = [line.split("-") for line in puzzle_input.splitlines()]
@@ -39,12 +69,14 @@ def part_2(puzzle_input: str) -> float:
     while incomplete_paths:
         current_path = incomplete_paths.pop()
         for destination in destinations[current_path[-1]]:
+            if destination == "start":
+                continue
             if destination == "end":
                 completed_paths.append(current_path + ["end"])
             elif (
                     destination.islower()
                     and destination in current_path
-                    and not current_path[0] == "doubled"
+                    and current_path[0] != "doubled"
             ):
                 incomplete_paths.append(["doubled"] + current_path + [destination])
             elif not destination.islower() or destination not in current_path:
@@ -54,10 +86,14 @@ def part_2(puzzle_input: str) -> float:
 
 def test_part_1():
     assert part_1(test_input) == 10
+    assert part_1(test_input_2) == 19
+    assert part_1(test_input_3) == 226
 
 
 def test_part_2():
     assert part_2(test_input) == 36
+    assert part_2(test_input_2) == 103
+    assert part_2(test_input_3) == 3509
 
 
 def main():
